69c271a25327bee437d1f641d10d0d8073b7b4c0
Tobias Berger <tobi.berger13@gmail.com>
2022-01-10 20:36:34 +0100

    Update AssemblyInfo and OutputPath

--- Source/RimVali Core/Properties/AssemblyInfo.cs
+++ Source/RimVali Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RimVali")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
