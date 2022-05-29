4eae34fc6da909d24ca58e4110650eacb04ce53e
Daniel Plaster <daniel_plaster@live.de>
2022-05-29 03:13:12 +0200

    implement QuestStages

--- Source/RimVali Core/QLine/QuestStage.cs
+++ Source/RimVali Core/QLine/QuestStage.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace RimValiCore.QLine
 {
     public class QuestStage
     {
-        public virtual void ExposeData()
-        {
+        public string label;
+        public string description;
+        public List<QuestStageButtonDecision> buttons;
 
+        public string LabelCap => label.CapitalizeFirst();
+    }
+
+    public class QuestStageButtonDecision
+    {
+        public  QuestStageButtonDecision(string buttonText, Action action)
+        {
+            this.buttonText = buttonText;
+            this.action = action;
         }
+
+        private string buttonText;
+        private Action action;
+
+        public string ButtonText { get => buttonText; set => buttonText = value; }
+        public Action Action { get => action; set => action = value; }
     }
 }
